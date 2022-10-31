a243f487693d24e20953a0c1fa748b8303d4b426
jplaulau14 <code.patricklaurel@gmail.com>
2022-10-31 22:11:26 +0800

    fixed preprocessing class

---
+++ linear_models.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+class linear_models:
+    class LinearRegression:
+        def __init__(self):
+            pass
+            
+        # TO DO
--- preprocessing.py
+++ preprocessing.py
@@ -1,28 +1,107 @@
 import numpy as np
 import pandas as pd
 
-class Preprocessing:
+
+class preprocessing:
     class MinMaxScaler:
-        def __init__(self, X):
-            self.X = X
+        def __init__(self):
+            pass
+
+        def fit(self, X):
+            """
+            Compute the range for later scaling
+            Args:
+                X: array-like shape
+            Return:
+                self: fitted object
+            """
             self.min = np.min(X, axis=0)
             self.max = np.max(X, axis=0)
             self.range = self.max - self.min
+            return self
 
         def transform(self, X):
+            """
+            Compute the min-max normalized column of X
+            Args:
+                X: array-like shape
+            Returns:
+                X: array-like shape with each column normalized by its z-score
+            """
             return (X - self.min) / self.range
 
+        def fit_transform(self, X):
+            """
+            Compute the min-max normalized column of X in one step
+            Args:
+                X: array-like shape
+            Returns:
+                X: array-like shape with each column normalized by its z-score
+            """
+            min = np.min(X, axis=0)
+            max = np.max(X, axis=0)
+            range = max - min
+
+            return (X - min) / range
+
         def inverse_transform(self, X):
+            """
+            Undo the scaling of X
+            Args:
+                X: array-like shape
+            Returns:
+                X: array-like shape
+            """
             return X * self.range + self.min
-        
+
     class StandardScaler:
-        def __init__(self, X):
-            self.X = X
+        def __init__(self):
+            pass
+
+        def fit(self, X):
+            """
+            Computes the mean and std for later scaling
+            Args:
+                X: array-like shape
+            Returns:
+                self: fitted object
+            """
             self.mean = np.mean(X, axis=0)
             self.std = np.std(X, axis=0)
+            return self
 
         def transform(self, X):
+            """
+            Compute the z-score normalized column of X
+            Args:
+                X: array-like shape
+            Returns:
+                X: array-like shape
+            """
             return (X - self.mean) / self.std
+        
+        def fit_transform(self, X):
+            """
+            Compute the z-score normalized column of X in one step
+            Args:
+                X: array-like shape
+            Returns:
+                X: array-like shape with each column normalized by its z-score
+            """
+            mean_ = np.mean(X, axis=0)
+            std_ = np.std(X, axis=0)
 
+            return (X - mean_) / std_
+        
         def inverse_transform(self, X):
+            """
+            Undo the scaling of X
+            Args:
+                X: array-like shape
+            Returns:
+                X: array-like shape
+            """
+
             return X * self.std + self.mean
+        
+    
